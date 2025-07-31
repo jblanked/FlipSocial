85dfb52b57ae446f981749e61ea0524b6a03a7e7
jblanked <82678820+jblanked@users.noreply.github.com>
2025-07-31 14:23:18 -0400

    remove C version

--- update/update.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <flip_social.h>
-#define BUILD_ID "6714d6326cb04a64f7ec2328"
-#define APP_ID "flip_social"
-#define FAP_ID "flip_social"
-#define APP_FOLDER "GPIO"
-#define MOM_FOLDER "FlipperHTTP"
-bool update_is_ready(FlipperHTTP *fhttp, bool use_flipper_api);
